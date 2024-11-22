5f249f50e56db13a984c098b1abcaa9467073dd1
unknown <designanimation.inc@gmail.com>
2024-11-22 13:23:42 -0300

    feat: backend communication with angular

--- angular-app/src/app/home/pages/meeting-room-page/meeting-room-page.component.ts
+++ angular-app/src/app/home/pages/meeting-room-page/meeting-room-page.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../services/user.service';
+import { UserDTO } from '../../../dto/user-dto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-meeting-room-page',
   standalone: true,
-  imports:[
-
+  imports: [
+    CommonModule,
   ],
   templateUrl: './meeting-room-page.component.html',
-  styleUrl: './meeting-room-page.component.css'
+  styleUrl: './meeting-room-page.component.css',
 })
-export class MeetingRoomPageComponent {
+
+export class MeetingRoomPageComponent implements OnInit {
+
+  constructor(private userService: UserService) {}
+    
+  users: UserDTO[] = [];
+  isLoading: boolean = false;
+  error: string | null = null;
+
+  ngOnInit(): void {
+    this.loadAll();
+  }
+
+  loadAll(): void {
+    this.isLoading = true;
+    this.error = null;
+    this.userService.findAll().subscribe({
+      next: (data) => {
+        this.users = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to load users';
+        console.error(err);
+        this.isLoading = false;
+      },
+    });
+  }
 
 }
