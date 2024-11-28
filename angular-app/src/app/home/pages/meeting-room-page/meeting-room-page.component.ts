4ad2a649ccc34e2d3288c231c50823ed88fd55f9
unknown <designanimation.inc@gmail.com>
2024-11-28 15:33:32 -0300

    feat: paginator meeting-room-page html and ts

--- angular-app/src/app/home/pages/meeting-room-page/meeting-room-page.component.ts
+++ angular-app/src/app/home/pages/meeting-room-page/meeting-room-page.component.ts
@@ -2,12 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../services/user.service';
 import { UserDTO } from '../../../dto/user-dto';
 import { CommonModule } from '@angular/common';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
+import { MatCard, MatCardContent, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
 
 @Component({
   selector: 'app-meeting-room-page',
   standalone: true,
   imports: [
     CommonModule,
+    InfiniteScrollDirective,
+    MatCard,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardContent,
+
   ],
   templateUrl: './meeting-room-page.component.html',
   styleUrl: './meeting-room-page.component.css',
@@ -15,33 +23,30 @@ import { CommonModule } from '@angular/common';
 
 export class MeetingRoomPageComponent implements OnInit {
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+  ) {}
     
   users: UserDTO[] = [];
-  isLoading: boolean = false;
-  error: string | null = null;
+
+  throttle = 0;
+  distance = 2;
   page = 0;
   size = 5;
 
   ngOnInit(): void {
-    this.loadAll();
+    this.userService
+      .findAll(this.page, this.size)
+      .subscribe((users => {
+        this.users = users.content;
+      }));
   }
-
-  loadAll(): void {
-    this.isLoading = true;
-    this.error = null;
-  
-    this.userService.findAll(this.page, this.size).subscribe({
-      next: (data) => {
-        this.users = data.content;
-        this.isLoading = false;
-      },
-      error: (err) => {
-        this.error = 'Failed to load users';
-        console.error(err);
-        this.isLoading = false;
-      },
-    });
+  onScroll(): void {
+    this.userService
+      .findAll(++this.page, this.size)
+      .subscribe((users => {
+        this.users.push(...users.content);
+      }));
   }
 
 }
