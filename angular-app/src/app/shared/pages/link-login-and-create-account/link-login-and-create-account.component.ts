d65ab67237da4094e528d088abb6ef148e3bbc3c
unknown <designanimation.inc@gmail.com>
2025-05-06 21:59:25 -0300

    [FEATURE] logout

--- angular-app/src/app/shared/pages/link-login-and-create-account/link-login-and-create-account.component.ts
+++ angular-app/src/app/shared/pages/link-login-and-create-account/link-login-and-create-account.component.ts
@@ -6,6 +6,10 @@ import Swal from 'sweetalert2';
 import { environment } from '../../../../environments/environment';
 import { HttpParams } from '@angular/common/http';
 import { AuthService } from '../../../users/services/auth.service';
+import { TokenService } from '../../../users/services/token.service';
+import * as CryptoJS from 'crypto-js';
+
+const CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 @Component({
     selector: 'app-link-login-and-create-account',
@@ -25,31 +29,54 @@ export class LinkLoginAndCreateAccountComponent implements OnInit{
       response_type: environment.response_type,
       response_mode: environment.response_mode,
       code_challenge_method: environment.code_challenge_method,
-      code_challenge: environment.code_challenge,
     };
     logout_url = environment.logout_url;
+    isLogged: boolean = false;
+    isUser: boolean = false;
 
-  constructor( private router: Router, private authService: AuthService) {}
+
+  constructor( private router: Router, private authService: AuthService, private tokenService: TokenService) {}
+
 
   ngOnInit(): void {
- 
+    this.getLogged();
   }
 
-  // get authenticated(){
-  //   return this.authService.authenticated();
-  // }
-
    onLogin(): void {
+    const code_verifier = this.generateCodeVerifier();
+    this.tokenService.setVerifier(code_verifier);
+    this.params.code_challenge = this.generateCodeChallenge(code_verifier);
      const htmlParams = new HttpParams({fromObject: this.params});
      const codeUrl = this.authorize_uri + htmlParams.toString();
      location.href = codeUrl;
-
    }
 
    onLogout(): void {
+    this.tokenService.clear()
     location.href = this.logout_url;
-
    }
-  
+
+   getLogged(): void {
+    this.isLogged = this.tokenService.isLogged();
+    this.isUser = this.tokenService.isUser();
+   }
+
+   generateCodeVerifier(): string {
+    let result = '';
+    const charactersLength = CHARACTERS.length;
+    for (let i = 0; i < 44; i++) {
+      result += CHARACTERS.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result
+   }
+
+   generateCodeChallenge(codeVerifier: string): string {
+    const codeVerifierHash = CryptoJS.SHA256(codeVerifier).toString(CryptoJS.enc.Base64);
+    const code_callenge = codeVerifierHash
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+    return code_callenge;
+   }
 
 }
--- angular-app/src/app/users/services/token.service.ts
+++ angular-app/src/app/users/services/token.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+import { environment } from '../../../environments/environment';
 
 const ACCESS_TOKEN = 'access_token';
 const REFRESH_TOKEN = 'refresh_token';
+const CODE_VERIFIER = 'code_verifier';
 
 @Injectable({
   providedIn: 'root'
@@ -30,6 +33,44 @@ export class TokenService {
     localStorage.removeItem(REFRESH_TOKEN);
   }
 
+  isLogged(): boolean {
+    return localStorage.getItem(ACCESS_TOKEN) !== null;
+  }
+
+  isUser(): boolean {
+    if(!this.isLogged()) return false;
+    const token = this.getAccessToken() as string;
+    const payload = token!.split('.')[1];
+    const decodedPayload = JSON.parse(atob(payload));
+    const roles = decodedPayload.roles;
+    if(roles.indexOf('ROLE_USER') < 0) return false;
+    return true;
+  }
+
+  setVerifier(code_verifier: string): void {
+    if(localStorage.getItem(CODE_VERIFIER)) {
+      this.deleteVerifier
+    }
+    const encrypted = CryptoJS.AES.encrypt(code_verifier, environment.secret_pkce);
+    localStorage.setItem(CODE_VERIFIER, encrypted.toString());
+  }
+
+
+  getVerifier(): string {
+    const encrypted = localStorage.getItem(CODE_VERIFIER);
+    if (!encrypted) {
+      return '';
+    }
+    const decrypted = CryptoJS.AES.decrypt(encrypted, environment.secret_pkce);
+    return decrypted.toString(CryptoJS.enc.Utf8);
+  }
+
+
+
+
+  deleteVerifier(): void {
+    localStorage.removeItem(CODE_VERIFIER);
+  }
 
   
 }
