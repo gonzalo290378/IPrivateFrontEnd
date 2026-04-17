8fff65629be5510b926b0a9f31239fb83ed8445d
gonzalo290378 <gonzalo29031978@hotmail.com>
2026-04-17 09:36:17 +0200

    fix angular project components V1.0.3

--- angular-app/src/app/users/services/user.service.ts
+++ angular-app/src/app/users/services/user.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
 import { catchError, Observable, of } from 'rxjs';
 import { UserDTO } from '../../dto/user-dto';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { UserFormDTO } from '../../dto/user-form-dto.model';
 import { User } from '../../models/user';
 import { UserDetailsFreeAreaDTO } from '../../dto/user-details-free-area-dto';
-import { TokenService } from './token.service';
-import { ResourceService } from './resource.service';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -15,11 +13,7 @@ import { environment } from '../../../environments/environment';
 export class UserService {
   private baseUrl = environment.msUsers;
 
-  constructor(
-    private http: HttpClient,
-    private tokenService: TokenService,
-    private resourceService: ResourceService,
-  ) {}
+  constructor(private http: HttpClient) {}
 
   findAll(page: number, size: number): Observable<any> {
     const params = new HttpParams().set('page', page).set('size', size);
@@ -93,30 +87,17 @@ export class UserService {
     username: string,
     userDetailsFreeAreaDTO: UserDetailsFreeAreaDTO,
   ): Observable<UserDetailsFreeAreaDTO | undefined> {
-    const token = this.tokenService.getAccessToken();
-    let headers = new HttpHeaders();
-    if (token) {
-      headers = headers.set('Authorization', `Bearer ${token}`);
-    }
-
     return this.http
       .put<UserDetailsFreeAreaDTO>(
         `${this.baseUrl}edit/${username}`,
         userDetailsFreeAreaDTO,
-        { headers },
       )
       .pipe(catchError((error) => of(undefined)));
   }
 
   updatePreferences(preferenceDTO: any): Observable<void> {
-    const token = this.tokenService.getAccessToken();
-    let headers = new HttpHeaders();
-    if (token) {
-      headers = headers.set('Authorization', `Bearer ${token}`);
-    }
-
     return this.http
-      .put<void>(`${this.baseUrl}preferences`, preferenceDTO, { headers })
+      .put<void>(`${this.baseUrl}preferences`, preferenceDTO)
       .pipe(
         catchError((error) => {
           console.error('Error updating preferences', error);
@@ -126,14 +107,7 @@ export class UserService {
   }
 
   getPreferences(): Observable<any> {
-    const token = this.tokenService.getAccessToken();
-
-    let headers = new HttpHeaders();
-    if (token) {
-      headers = headers.set('Authorization', `Bearer ${token}`);
-    }
-
-    return this.http.get(`${this.baseUrl}preferences`, { headers });
+    return this.http.get(`${this.baseUrl}preferences`);
   }
 
   checkUsernameAvailability(
@@ -151,30 +125,16 @@ export class UserService {
   }
 
   deleteUser(id: number): Observable<UserDetailsFreeAreaDTO | undefined> {
-    const token = this.tokenService.getAccessToken();
-    let headers = new HttpHeaders();
-    if (token) {
-      headers = headers.set('Authorization', `Bearer ${token}`);
-    }
-
     return this.http
-      .delete<UserDetailsFreeAreaDTO>(`${this.baseUrl}delete/${id}`, {
-        headers,
-      })
+      .delete<UserDetailsFreeAreaDTO>(`${this.baseUrl}delete/${id}`)
       .pipe(catchError((error) => of(undefined)));
   }
 
   reactivateUser(id: number) {
-    const token = this.tokenService.getAccessToken();
-    let headers = new HttpHeaders();
-    if (token) {
-      headers = headers.set('Authorization', `Bearer ${token}`);
-    }
     return this.http
       .put<UserDetailsFreeAreaDTO>(
         `${this.baseUrl}${id}/reactivate`,
         {},
-        { headers },
       )
       .pipe(catchError((error) => of(undefined)));
   }
